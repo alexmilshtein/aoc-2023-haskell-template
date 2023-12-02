ef10144cf65e1d1941253d9a3ea14a429313fc9a
Alex Milshtein <alex@milshtein.net>
2023-12-02 17:16:43 +1100

    Solve day 1

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,10 +1,19 @@
 module Day01 where
 
 import Paths_aoc2023 (getDataFileName)
+import Data.Char (isDigit)
+
+filterDigits :: String -> String
+filterDigits = filter isDigit
+
+firstAndLast:: String -> Int
+firstAndLast [x] = read [x,x]
+firstAndLast xs = read [head xs, last xs]
 
 day01 :: IO ()
 day01 = do
   inputLines <- lines <$> (getDataFileName "day01-input.txt" >>= readFile)
   putStrLn "This is what I read from input:"
   putStrLn $ unlines inputLines
-  putStrLn "TODO: implement Day 01"
+  let res = sum $ firstAndLast . filterDigits <$> inputLines
+  print $ mconcat ["Result: ", show res]
