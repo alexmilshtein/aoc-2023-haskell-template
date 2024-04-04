58d0be8775f9e5f101e9bd74a4a9db26f4a42d34
Alex Milshtein <alex@milshtein.net>
2024-04-04 21:56:40 +1100

    fix: cleanup a bit

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,29 +1,28 @@
 module Day01 where
 
-import Paths_aoc2023 (getDataFileName)
 import Data.Char (isDigit)
+import Paths_aoc2023 (getDataFileName)
 
 convertDigitWords :: String -> String
 convertDigitWords = go []
-      where go result [] = result
-            go result xs = case xs of
-                  ('o':'n':'e':ys) -> go (result ++ "1") (take 2 (tail xs) ++ ys)
-                  ('t':'w':'o':ys) -> go (result ++ "2") (take 2 (tail xs) ++ ys)
-                  ('t':'h':'r':'e':'e':ys) -> go (result ++ "3") (take 4 (tail xs) ++ ys)
-                  ('f':'o':'u':'r':ys) ->  go (result ++ "4") (take 3 (tail xs) ++ ys)
-                  ('f':'i':'v':'e':ys) -> go (result ++ "5") (take 3 (tail xs) ++ ys)
-                  ('s':'i':'x':ys) -> go (result ++ "6") (take 2 (tail xs) ++ ys)
-                  ('s':'e':'v':'e':'n':ys) -> go (result ++ "7") (take 4 (tail xs) ++ ys)
-                  ('e':'i':'g':'h':'t':ys) -> go (result ++ "8") (take 4 (tail xs) ++ ys)
-                  ('n':'i':'n':'e':ys) -> go (result ++ "9") (take 3 (tail xs) ++ ys)
-                  (y:ys) -> go (result ++ [y]) ys
-
-
+  where
+    go result [] = result
+    go result xs = case xs of
+      ('o' : 'n' : 'e' : _) -> go (result ++ "1") (drop 2 xs)
+      ('t' : 'w' : 'o' : _) -> go (result ++ "2") (drop 2 xs)
+      ('t' : 'h' : 'r' : 'e' : 'e' : _) -> go (result ++ "3") (drop 4 xs)
+      ('f' : 'o' : 'u' : 'r' : ys) -> go (result ++ "4") ys
+      ('f' : 'i' : 'v' : 'e' : _) -> go (result ++ "5") (drop 3 xs)
+      ('s' : 'i' : 'x' : ys) -> go (result ++ "6") ys
+      ('s' : 'e' : 'v' : 'e' : 'n' : _) -> go (result ++ "7") (drop 4 xs)
+      ('e' : 'i' : 'g' : 'h' : 't' : _) -> go (result ++ "8") (drop 4 xs)
+      ('n' : 'i' : 'n' : 'e' : _) -> go (result ++ "9") (drop 3 xs)
+      (y : ys) -> go (result ++ [y]) ys
 
 filterDigits :: String -> String
 filterDigits = filter isDigit
 
-firstAndLast:: String -> Int
+firstAndLast :: String -> Int
 firstAndLast xs = read [head xs, last xs]
 
 day01 :: IO ()
